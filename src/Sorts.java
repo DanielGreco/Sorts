08786005175095f20b406949754fe3b6f246d1e3
Daniel Greco <dgreco5360@bths.edu>
2018-11-20 14:16:43 -0500

    Add .gitignore file.

--- src/Sorts.java
+++ src/Sorts.java
@@ -30,15 +30,57 @@ public class Sorts
                         swap(i, lowPosition, arr);
                     }
             }
+        public static void ascendingSort(String[] arr)
+            {
+                for (int i=0; i<arr.length-1; i++)
+                    {
+                        int lowPosition=i;
+                        for (int j=i+1; j<arr.length; j++)
+                            {
+                                if (arr[j].compareTo(arr[lowPosition])<0)
+                                    {
+                                        lowPosition=j;
+                                    }
+                            }
+                        swap(i, lowPosition, arr);
+                    }
+            }
+        public static void descendingSort(String[] arr)
+            {
+                for (int i=0; i<arr.length-1; i++)
+                    {
+                        int lowPosition=i;
+                        for (int j=i+1; j<arr.length; j++)
+                            {
+                                if (arr[j].compareTo(arr[lowPosition])>0)
+                                    {
+                                        lowPosition=j;
+                                    }
+                            }
+                        swap(i, lowPosition, arr);
+                    }
+            }
         private static void swap(int indexA, int indexB, int[] arr)
             {
                 int temp=arr[indexA];
                 arr[indexA]=arr[indexB];
                 arr[indexB]=temp;
             }
+        private static void swap(int indexA, int indexB, String[] arr)
+            {
+                String temp=arr[indexA];
+                arr[indexA]=arr[indexB];
+                arr[indexB]=temp;
+            }
         public static void printArr(int[] arr)
             {
                 for (int num : arr)
+                    System.out.print(num + " ");
+                System.out.println();
+            }
+        public static void printArr(String[] arr)
+            {
+                for (String num : arr)
                     System.out.print(num + " ");
                 System.out.println();
             }
--- src/Tester.java
+++ src/Tester.java
@@ -2,16 +2,25 @@ public class Tester
     {
         public static void main(String args[])
             {
-               int [] arr1 = {328,812,1019,519,1011,408,406,214,1004,222,102,816,204,601,312,713,604};
-               Sorts.printArr(arr1);
-               System.out.println();
-
-               Sorts.ascendingSort(arr1);
-               Sorts.printArr(arr1);
-               System.out.println();
-
-                Sorts.descendingSort(arr1);;
+                int[] arr1 = {328,812,1019,519,1011,408,406,214,1004,222,102,816,204,601,312,713,604};
                 Sorts.printArr(arr1);
+                System.out.println();
+
+                Sorts.ascendingSort(arr1);
+                Sorts.printArr(arr1);
+                System.out.println();
+
+                Sorts.descendingSort(arr1);
+                Sorts.printArr(arr1);
+                System.out.println();
+
+                String[] arr2 = {"Daniel","Cindy","Andrew","Sheba","Chrissie","Calvin","Yashrif","Awais","Angela"};
+                Sorts.ascendingSort(arr2);
+                Sorts.printArr(arr2);
+                System.out.println();
+
+                Sorts.descendingSort(arr2);
+                Sorts.printArr(arr2);
                 System.out.println();
             }
     }
